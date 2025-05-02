d745d7d41c1f68e421603ea8406a0f6c7803287a
candyliaa <astronomy.dev@gmail.com>
2025-05-02 17:39:20 +0300

    updated index.js again

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Why is it always Hello World!, and never How is World? :(</h1>')
+  res.send('<h1>Why is it always Hello World!, and never How is World?! :(</h1>')
 })
 
 const PORT = 8080
